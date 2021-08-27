c885287a84785efc85f958b482c73826ed93e664
rzhasibuan <rzhasibuan@gmail.com>
2021-08-27 16:47:18 +0700

    add notes and get notes

---
+++ src/services/inMemory/NotesService.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-underscore-dangle */
+const { nanoid } = require('nanoid');
+
+class notesServices {
+  constructor() {
+    this._notes = [];
+  }
+
+  addNote({ title, body, tags }) {
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const newNote = {
+      title, tags, body, id, createdAt, updatedAt,
+    };
+    this._notes.push(newNote);
+
+    const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
+
+    if (!isSuccess) {
+      throw new Error('Catatan gagal ditambahkan');
+    }
+    return id;
+  }
+
+  getNotes() {
+    return this._notes;
+  }
+}
